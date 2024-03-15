fb63efc00e741059a1bd41a5fa9fd053ba299874
Sagar Yadav <sagardxd5@gmail.com>
2024-03-15 23:44:37 +0530

    signup royte

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,5 @@
 import express, { Router, Request, Response } from 'express';
-import { signInSchema } from '../zod/user';
+import { signInSchema, signUpSchema } from '../zod/user';
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
@@ -28,5 +28,35 @@ userRouter.post("/signin", async (req: Request, res: Response) => {
         return res.status(500).json({ error: "Internal server error" });
     }
 })
+
+
+userRouter.post("/signup", async (req: Request, res: Response) => {
+    //zod validation
+    const result = signUpSchema.safeParse(req.body);
+    if (!(result.success)) {
+        return res.status(411).json({ msg: "Input errors", error: result.error });
+    }
+
+    try {
+
+        // Create a new user
+        const newUser = await prisma.user.create({
+            data: {
+                email: result.data.email,
+                password: result.data.password,
+                name: result.data.name,
+            },
+        })
+
+        return res.status(200).json("new user created");
+    } catch (error) {
+        console.error("Error occurred while signing in:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+})
+
+
+
+
 
 export default userRouter;
