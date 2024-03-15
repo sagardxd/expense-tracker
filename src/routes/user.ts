1f026c073d4f794cf8ffc4f8b178adde59ee51e8
Sagar Yadav <sagardxd5@gmail.com>
2024-03-15 23:41:41 +0530

    signin route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,7 @@
 import express, { Router, Request, Response } from 'express';
 import { signInSchema } from '../zod/user';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const userRouter = Router();
 
@@ -9,14 +11,22 @@ userRouter.post("/signin", async (req: Request, res: Response) => {
     if (!(result.success)) {
         return res.status(411).json({ msg: "Input errors", error: result.error });
     }
-    
 
     try {
-        const 
-    } catch (error) {
-        
-    }
+        const existingUser = prisma.user.findUnique({
+            where: {
+                email: result.data.email
+            }
+        })
+        if (!existingUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
 
+        return res.status(200).json(existingUser);
+    } catch (error) {
+        console.error("Error occurred while signing in:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
 })
 
 export default userRouter;
